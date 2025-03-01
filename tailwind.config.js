b283dc5bce509a8829bca0ad511e1b64fff97636
Joey Cruz <typicaltony0824@gmail.com>
2025-03-02 05:25:05 +0800

    new site

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        colorBlue: '#3873f3',
+      },
+      fontFamily: {
+        fredoka: ["Fredoka", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
